3fa4d04e7daf421f8a91a6821fd228faf72f91a7
ismail demir <ismi229381765@gmail.com>
2023-04-11 02:08:56 +0300

    ders 3

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
+
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
+
+
 const app = express();
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//template engine
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  // async - await yapısı kullanacğız.
+  await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  res.redirect('/');
 });
 
 const port = 3000;
