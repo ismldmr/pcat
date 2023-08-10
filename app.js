3448aa097f02883551db54db545d2e9a0d14d240
ismail demir <ismi229381765@gmail.com>
2023-08-10 20:20:20 +0300

    uploading

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const ejs = require('ejs');
+const fs= require('fs')
 const Photo = require('./models/Photo');
 
+// modülü kullanıma alıyoruz.
 
 const app = express();
 mongoose.connect('mongodb://localhost/pcat-test-db', {
@@ -17,38 +19,50 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
 //routes
+
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
+  const photos = await Photo.find({});
   res.render('index', {
-    photos
+    photos,
   });
-  app.get('/photos/:id', async(req, res) => {
+  app.get('/photos/:id', async (req, res) => {
     //console.log(req.params.id)
     //res.render('about');
-    const photo = await Photo.findById(req.params.id)
-    res.render('photo',{ photo
-
-    })
+    const photo = await Photo.findById(req.params.id);
+    res.render('photo', { photo });
   });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
-  
   res.render('add');
 });
 
-app.post('/photos', async (req, res) => {
-  // async - await yapısı kullanacğız.
-  await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
-  res.redirect('/');
-});
 
+app.post('/photos', async (req, res) => {
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+  
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+});
 const port = 3000;
 
 app.listen(port, () => {
